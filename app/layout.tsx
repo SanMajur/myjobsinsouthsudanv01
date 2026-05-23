00c768bc064838d40eb0b7d6afdb8b0923b3c478
Santino Majur <demalong22@gmail.com>
2026-05-23 18:18:45 +0200

    feat: add universal Footer to layout
    Shows on all pages. v0.1 - Built in Juba with WhatsApp contact

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Footer from "@/components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,10 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col max-w-3xl mx-auto py-8">
+        {children}
+        <Footer  />
+      </body>
     </html>
   );
 }
