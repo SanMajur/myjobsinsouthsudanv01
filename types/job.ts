780638541fa8ef9dd2b1b59b6fedb623bae15532
Santino Majur <demalong22@gmail.com>
2026-05-23 17:41:46 +0200

    chore: add Job type with optional salary

--- types/job.ts
+++ types/job.ts
@@ -1,9 +1,10 @@
 export interface Job {
-  id: string;
+  id: number;
   title: string;
   company: string;
   location: string;
   postedAt: Date;
   isRemote: boolean; 
-  employmentType?: "Full-time" | "Part-time" | "Contract" | "Internship";
+  employmentType: "Full-time" | "Part-time" | "Contract" | "Internship";
+  salary?: number;
 }
