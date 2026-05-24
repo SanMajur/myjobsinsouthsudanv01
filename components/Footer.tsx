7f9fe28ad86b6ee3534542342a9cd00a5d588f6f
Santino Majur <demalong22@gmail.com>
2026-05-24 15:31:22 +0200

    style: upgrade Footer  with Tailwind center

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="text-xs text-gray-400 mt-8">
+    <footer className="text-xs text-gray-400 mt-8 p-4 text-center">
   v0.1 - Built in Juba. Testing with real job seekers. 
   Design updates weekly. Report bugs: WhatsApp +211 921880822
 </footer>
