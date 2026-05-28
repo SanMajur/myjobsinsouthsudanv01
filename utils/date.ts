1aa719f3d41f548be87e9300504bcdf5093be038
Santino Majur <demalong22@gmail.com>
2026-05-28 10:20:45 +0200

    merge: sync with existing GitHub repo
    Pulled remote history with --allow-unrelated-histories.
    Connected local Indeed redesign to existing origin.

--- utils/date.ts
+++
@@ -1,5 +0,0 @@
-export function getDaysAgo(date: Date): number { // function is type of number because it returns the number of days ago
-  const msPerDay = 1000 * 60 * 60 * 24 // number of milliseconds in a day 86400000 milliseconds in a day
-  const diff = Date.now() - date.getTime() 
-  return Math.floor(diff / msPerDay)
-}
